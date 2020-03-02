2228aaf16a3e3e1a76a00f9bf86ae476f60016be
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-03-02 14:27:14 +0100

    rpi3: inherit from common tree

---
+++ lineage_rpi3.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+
+# Inherit from rpi4 device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+include frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk
+
+PRODUCT_DEVICE := rpi3
+PRODUCT_NAME := lineage_rpi3
+PRODUCT_BRAND := arpi
+PRODUCT_MODEL := Raspberry Pi 3
+PRODUCT_MANUFACTURER := ARPI
+PRODUCT_CHARACTERISTICS := tablet
